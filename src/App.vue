e25041face68fd67677626d0267e51f275906145
canberk <canberkkoc.94@gmial.com>
2020-11-22 11:05:34 +0300

    Book Search App

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="search-box w-25">        
+        <b-input-group>
+          <b-form-input v-model="book_query" @keyup.enter ="fetchBooks"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="info" @click="fetchBooks"><b-icon icon="Search"></b-icon></b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </div>
+              <div class="cards">
+          <b-card v-for="(item,index ) in books.items" :key="index">
+            <b-card-img :src="item.volumeInfo.imageLinks.thumbnail" alt="Image" ></b-card-img>
+            <b-card-title>{{item.volumeInfo.title}}</b-card-title>
+            <b-card-text v-if="item.volumeInfo.description == ' '">
+               Description not define
+            </b-card-text>
+            <b-card-text v-else>
+              {{item.volumeInfo.description | sizeChar(100)}}
+            </b-card-text>
+
+            <b-button :href="item.volumeInfo.canonicalVolumeLink" variant="primary" target="_blank">Go Link</b-button>
+          </b-card>
+        </div>
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      book_query:"",
+      api_base:"https://www.googleapis.com/books/v1/",
+      api_key:"AIzaSyCgA_tmwaj7OsYqrf8UuJPHFulcfSgPnhU",
+      books:{},
+      //
+    }
+  },
+  methods:{
+    // get all book data
+    fetchBooks(){
+      fetch(`${this.api_base}volumes?q=${this.book_query}&key=${this.api_key}`)
+      .then(res =>{
+        return res.json()
+      }).then(this.ShowResults)
+    },
+    ShowResults(results){
+      this.books = results;
+      console.log(this.books)
+    }
+  },
+  filters:{
+    //character restriction
+    sizeChar: function(value, size){
+        if(!value) return '';
+        value = value.toString();
+
+        if(value.length <= size){
+          return value
+        }
+        return value.substr(0, size) + '...';
+    }
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +80,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.search-box{
+  margin: 0 auto;
+}
+.cards{
+  margin:3.5rem ;
+  grid-gap: 5rem;
+  display: grid;
+  grid-template-columns: repeat(5 , 1fr);
 }
 </style>
